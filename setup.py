0ad7108696f23312dfbd7772899166070bd43b08
Peter Nociar <peter.nociar@digital.trade.gov.uk>
2017-06-02 12:14:57 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='dit-sso-client',
-    version='0.1',
+    version='0.1.1',
     packages=['ditsso'],
     include_package_data=True,
     license='BSD License',  # example license
