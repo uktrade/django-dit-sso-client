107ade5396f911a8cd316ce47e2d72c3e35ec5d9
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2017-09-21 12:29:09 +0100

    Add ditsso_internal to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='dit-sso-client',
     version='0.1.2',
-    packages=['ditsso'],
+    packages=['ditsso', 'ditsso_internal'],
     include_package_data=True,
     license='BSD License',  # example license
     description='A simple Django app to integrate with DIT SSO provider.',
